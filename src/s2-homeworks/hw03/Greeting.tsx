a68914726c0d436b7db276b8dd3b18a127cc1512
Vecsel <penkov.voldemor@yandex.ru>
2022-12-10 18:10:42 +0300

    completed hw3

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, KeyboardEvent, SetStateAction} from 'react'
+import React, {ChangeEvent, KeyboardEvent, } from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass = error? s.errorInput:'' // need to fix with (?:)
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
