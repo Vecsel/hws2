9632a7932af04a3bfea7be4960190abc916ef15f
Vecsel <penkov.voldemor@yandex.ru>
2023-02-04 14:29:50 +0300

    completed hw9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -32,8 +32,8 @@ function Clock() {
     }
 
     const stringTime = date?.toLocaleTimeString() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date?.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
+    const stringDate = date?.toLocaleDateString('ru-Ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    console.log(typeof stringDate)
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 
     const stringDay = new Intl.DateTimeFormat('en-US',{weekday:'long'}).format(date) || <br/> // пишут студенты
