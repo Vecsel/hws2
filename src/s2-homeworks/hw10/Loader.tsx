787922c6b0d263d4e6fc413b22c4d97313642f16
Vecsel <penkov.voldemor@yandex.ru>
2023-02-12 17:27:59 +0300

    completed hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () =>
+    <div className={s.loader}>
+        <div className={s.white}></div>
+    </div>
