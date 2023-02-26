bb35877f81ac11533a0bdd938d716a46daa09d7b
Vecsel <penkov.voldemor@yandex.ru>
2023-02-26 12:28:55 +0300

    completed hw11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -17,7 +17,7 @@ function HW11() {
 
     const change = (event: Event, value: number|number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-        if (value instanceof Array) {
+        if (Array.isArray(value)) {
             setValue1(value[0])
             setValue2(value[1])
         } else {
